75765a0f146c1d00595a371d025730a196d30564
Tuong1994 <nhambontuong68@gmail.com>
2025-07-30 19:35:53 +0700

    update react code Typograph finished

--- src/features/UI/Typography/components/Title/TitleApi/TitleApi.vue
+++ src/features/UI/Typography/components/Title/TitleApi/TitleApi.vue
@@ -8,7 +8,7 @@ import useLangStore from '@/stores/LangStore'
 
 const t = useLangStore()
 
-const dataSource = computed<ComponentApi[]>(() => [
+const dataVue = computed<ComponentApi[]>(() => [
   {
     id: 'level',
     props: 'level',
@@ -109,6 +109,108 @@ const dataSource = computed<ComponentApi[]>(() => [
     default: '-'
   }
 ])
+
+const dataReact = computed<ComponentApi[]>(() => [
+  {
+    id: 'level',
+    props: 'level',
+    desc: t.lang.typography.api.titleDesc.level,
+    type: {
+      elType: ECodeElType.DEFAULT,
+      codes: ['1', '2', '3', '4', '5', '6']
+    },
+    required: t.lang.common.required.no,
+    default: '1'
+  },
+  {
+    id: 'aligns',
+    props: 'aligns',
+    desc: t.lang.typography.api.titleDesc.aligns,
+    type: {
+      elType: ECodeElType.DEFAULT,
+      codes: ['left', 'center', 'right', 'justify']
+    },
+    required: t.lang.common.required.no,
+    default: 'left'
+  },
+  {
+    id: 'variant',
+    props: 'variant',
+    desc: t.lang.typography.api.titleDesc.variant,
+    type: {
+      elType: ECodeElType.DEFAULT,
+      codes: ['default', 'secondary', 'success', 'warning', 'danger']
+    },
+    required: t.lang.common.required.no,
+    default: 'default'
+  },
+  {
+    id: 'weight',
+    props: 'weight',
+    desc: t.lang.typography.api.titleDesc.weight,
+    type: {
+      elType: ECodeElType.DEFAULT,
+      codes: ['number']
+    },
+    required: t.lang.common.required.no,
+    default: '500'
+  },
+  {
+    id: 'italic',
+    props: 'italic',
+    desc: t.lang.typography.api.titleDesc.italic,
+    type: {
+      elType: ECodeElType.DEFAULT,
+      codes: ['boolean']
+    },
+    required: t.lang.common.required.no,
+    default: '-'
+  },
+  {
+    id: 'underline',
+    props: 'underline',
+    desc: t.lang.typography.api.titleDesc.underline,
+    type: {
+      elType: ECodeElType.DEFAULT,
+      codes: ['boolean']
+    },
+    required: t.lang.common.required.no,
+    default: '-'
+  },
+  {
+    id: 'remove',
+    props: 'remove',
+    desc: t.lang.typography.api.titleDesc.remove,
+    type: {
+      elType: ECodeElType.DEFAULT,
+      codes: ['boolean']
+    },
+    required: t.lang.common.required.no,
+    default: '-'
+  },
+  {
+    id: 'rootClassName',
+    props: 'rootClassName',
+    desc: t.lang.typography.api.titleDesc.rootClassName,
+    type: {
+      elType: ECodeElType.DEFAULT,
+      codes: ['string']
+    },
+    required: t.lang.common.required.no,
+    default: '-'
+  },
+  {
+    id: 'rootStyle',
+    props: 'style',
+    desc: t.lang.typography.api.titleDesc.rootStyle,
+    type: {
+      elType: ECodeElType.DEFAULT,
+      codes: ['React.CSSProperties']
+    },
+    required: t.lang.common.required.no,
+    default: '-'
+  }
+])
 </script>
 
 <template>
@@ -116,6 +218,8 @@ const dataSource = computed<ComponentApi[]>(() => [
     title="Title"
     rootClassName="section-space"
     :id="typographyCatalogIds.TITLE_API"
-    :dataSource="dataSource"
+    :dataSource="[]"
+    :dataVue="dataVue"
+    :dataReact="dataReact"
   />
 </template>
